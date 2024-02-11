1cbfc463b03547e39658c33ec6641cb6e75437f6
Horvath Milan <ligerr13@gmail.com>
2024-02-11 20:54:05 +0100

    handle_navbar_button_backgorund_signal signal

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from src.main_window import Ui_MainWindow
 import qdarktheme
+from PyQt6.QtCore import QObject, pyqtSignal
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
 
 class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.pushButton_2.clicked.connect(self.the_button_was_clicked)
+        self.prev_button = None
 
-    def the_button_was_clicked(self):
-            print(self, " Signal Test")
+        self.ui.pushButton.clicked.connect(self.handle_navbar_button_backgorund_signal)
+        self.ui.pushButton_2.clicked.connect(self.handle_navbar_button_backgorund_signal)
+        self.ui.pushButton_3.clicked.connect(self.handle_navbar_button_backgorund_signal)
+
+    def handle_navbar_button_backgorund_signal(self):
+            button = self.sender()
+            
+            button.setStyleSheet("background-color: #33ccff")
+            
+            if self.prev_button is not None:
+                self.prev_button.setStyleSheet("background-color: transparent, border: 0px")
+            
+            self.prev_button = button
 
 if __name__ == "__main__":
     app = QApplication([])
