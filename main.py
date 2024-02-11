c21ce9f58e769b150a6bac75176107eb4b1022d8
Horvath Milan <ligerr13@gmail.com>
2024-02-11 18:13:30 +0100

    Added Theme

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from src.main_window import Ui_MainWindow
+import qdarktheme
 
 class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.pushButton_2.clicked.connect(self.the_button_was_clicked)
+
+    def the_button_was_clicked(self):
+            print(self, " Signal Test")
 
 if __name__ == "__main__":
     app = QApplication([])
+    app.setStyleSheet(qdarktheme.load_stylesheet())
+
     window = MyApp()
     window.show()
     app.exec()
