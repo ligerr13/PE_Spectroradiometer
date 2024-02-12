196098a47112e1002cde72343175045177c6ae1f
Horváth Milán <horvathmilan@Horvath-MacBook-Air.local>
2024-02-12 20:08:30 +0100

    Some changes

--- src/navbar.py
+++ src/navbar.py
@@ -1,24 +1,24 @@
 from PyQt6.QtCore import QObject
 from PyQt6.QtWidgets import QPushButton
 
-from src.resource_1 import Ui_MainWindow
-
-
 
 class NavBar(QObject):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.parent = parent
-
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(parent)
+    def __init__(self, resource):
+        super().__init__()
+        self.ui = resource
 
         self.prev_button = None
 
         #Signals and Connections
         self.ui.pushButton.clicked.connect(lambda: self.handle_navbar_button_background_signal(self.ui.pushButton))
+        self.ui.pushButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
+
         self.ui.pushButton_2.clicked.connect(lambda: self.handle_navbar_button_background_signal(self.ui.pushButton_2))
+        self.ui.pushButton_2.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(2))
+
         self.ui.pushButton_3.clicked.connect(lambda: self.handle_navbar_button_background_signal(self.ui.pushButton_3))
+        self.ui.pushButton_3.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(1))
+
 
     def handle_navbar_button_background_signal(self, button):
         if button == self.prev_button:
@@ -28,5 +28,5 @@ class NavBar(QObject):
             if self.prev_button is not None:
                 self.prev_button.setStyleSheet("border: 0px")
             
-            button.setStyleSheet("background-color: #3366cc; border: 2px solid white")
+            button.setStyleSheet("background-color: rgba(0, 0, 0, 50); border: 0px")
             self.prev_button = button
