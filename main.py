cc51aa49cfff01f7c60592d5a06422a72d2f97da
Horvath Milan <ligerr13@gmail.com>
2024-02-17 15:50:08 +0100

    Slots and signals changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from src.resource_1 import Ui_MainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QAbstractButton
+from src.objects.resource_1 import Ui_MainWindow
 from src.navbar import NavBar
 from src.nodeboard import NodeBoard
 import qdarktheme
@@ -19,16 +19,30 @@ class MyApp(QMainWindow):
 
 
 
+    #MainWindow Slots
+    def HandleDragMode(self, drag: bool):
+        print(self.sender(), drag)
+        
+        if drag is not None:
+            if  drag == False:
+                self.nodeboard.view.setDragMode(QGraphicsView.DragMode.NoDrag)
+            else:
+                self.nodeboard.view.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
 
+    def OnNodeboardSceneButtonClicked(self, Button):
+        if Button == self.nodeboard.prev_button:
+            self.nodeboard.prev_button.setStyleSheet("QPushButton {border: 0px;}QPushButton:hover {background: rgb(55, 55, 55);}")
+            self.nodeboard.prev_button = None
+        else:
+            if self.nodeboard.prev_button is not None:
+                self.nodeboard.prev_button.setStyleSheet("QPushButton {border: 0px;}QPushButton:hover {background: rgb(55, 55, 55);}")
+            Button.setStyleSheet("QPushButton {border: 0px;background-color: rgb(63, 101, 255);}, QPushButton:hover {background: rgb(55, 55, 55);}")
+            self.nodeboard.prev_button = Button        
+        
     def OnNavbarButtonClicked(self, pageId: int):
         self.ui.stackedWidget.setCurrentIndex(pageId)
-    
 
 
-
-
-
-    
 if __name__ == "__main__":
     app = QApplication([])
     app.setStyleSheet(qdarktheme.load_stylesheet("dark"))
