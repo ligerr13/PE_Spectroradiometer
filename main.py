7a3ed1a7608d8ba9fdce6e5929f1208650102e40
Horvath Milan <ligerr13@gmail.com>
2024-02-17 16:35:02 +0100

    Added HandleSelectMode

--- main.py
+++ main.py
@@ -20,25 +20,22 @@ class MyApp(QMainWindow):
 
 
     #MainWindow Slots
-    def HandleDragMode(self, drag: bool):
-        print(self.sender(), drag)
-        
+    def HandleDragMode(self, drag: bool):        
         if drag is not None:
             if  drag == False:
                 self.nodeboard.view.setDragMode(QGraphicsView.DragMode.NoDrag)
+                self.sender().setStyleSheet("QPushButton {border: 0px;}QPushButton:hover {background: rgb(55, 55, 55);}")
             else:
                 self.nodeboard.view.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
+                self.sender().setStyleSheet("QPushButton {border: 0px;background-color: rgb(63, 101, 255);}, QPushButton:hover {background: rgb(55, 55, 55);}")
 
-    def OnNodeboardSceneButtonClicked(self, Button):
-        if Button == self.nodeboard.prev_button:
-            self.nodeboard.prev_button.setStyleSheet("QPushButton {border: 0px;}QPushButton:hover {background: rgb(55, 55, 55);}")
-            self.nodeboard.prev_button = None
-        else:
-            if self.nodeboard.prev_button is not None:
-                self.nodeboard.prev_button.setStyleSheet("QPushButton {border: 0px;}QPushButton:hover {background: rgb(55, 55, 55);}")
-            Button.setStyleSheet("QPushButton {border: 0px;background-color: rgb(63, 101, 255);}, QPushButton:hover {background: rgb(55, 55, 55);}")
-            self.nodeboard.prev_button = Button        
-        
+    def HandleSelectMode(self, select: bool):
+        if select is not None:
+            if  select == False:
+                self.sender().setStyleSheet("QPushButton {border: 0px;}QPushButton:hover {background: rgb(55, 55, 55);}")
+            else:
+                self.sender().setStyleSheet("QPushButton {border: 0px;background-color: rgb(63, 101, 255);}, QPushButton:hover {background: rgb(55, 55, 55);}")
+
     def OnNavbarButtonClicked(self, pageId: int):
         self.ui.stackedWidget.setCurrentIndex(pageId)
 
