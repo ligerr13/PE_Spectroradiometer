ba651756548342fbb1e836b635b3cc8e75ac265e
Horvath Milan <ligerr13@gmail.com>
2024-02-18 18:14:02 +0100

    Created NodeboardSignalBus

---
+++ src/signals/nodeboardsignalbus.py
@@ -0,0 +1,19 @@
+from PyQt6.QtCore import QObject, pyqtSignal
+from PyQt6.QtWidgets import QGraphicsProxyWidget
+
+class NodeBoardSignalBus(QObject):
+    widgetSelectedSignal = pyqtSignal(QGraphicsProxyWidget)
+    widgetDeselectedSignal = pyqtSignal(QGraphicsProxyWidget)
+    widgetDeletedSignal = pyqtSignal(QGraphicsProxyWidget)
+
+    def __init__(self):
+        super().__init__()
+
+    def onWidgetSelectedSignalEmit(self, widget):
+        self.widgetSelectedSignal.emit(widget)
+
+    def onWidgetDeselectedSignalEmit(self, widget):
+        self.widgetDeselectedSignal.emit(widget)
+
+    def onWidgetDeletedSignalEmit(self, widget):
+        self.widgetDeletedSignal.emit(widget)
