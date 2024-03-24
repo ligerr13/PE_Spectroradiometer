f620a56e1ef44b83675b2fc363151d1f48ad5222
Horvath Milan <ligerr13@gmail.com>
2024-03-24 15:52:51 +0100

    Added Savefile editText

--- src/dialogs/measureDialog.py
+++ src/dialogs/measureDialog.py
@@ -1,7 +1,22 @@
 from PyQt6.QtWidgets import QDialog
 from src.objects.measure_dialog_1 import Ui_Dialog
-from src.instrument.command import 
+from src.instrument.command import ExecuteProgram, RMTS, MSWE, MEAS, MEDR
+from src.instrument.command import DataMode, DataFormat, ModeSelect, SpectralRange
 import asyncio
+
+
+basic_measure_program = [
+    RMTS(switch = ModeSelect.ENABLED),
+    MSWE(switch = ModeSelect.DISABLED),
+    MEAS(switch = ModeSelect.ENABLED),
+    MEDR(data_mode = DataMode.MEASUREMENT_CONDITIONS, data_format = DataFormat.ALPHANUMERIC),
+    MEDR(data_mode = DataMode.SPECTRAL_DATA, data_format = DataFormat.ALPHANUMERIC, spectral_range = SpectralRange.RANGE_380_TO_479),
+    MEDR(data_mode = DataMode.SPECTRAL_DATA, data_format = DataFormat.ALPHANUMERIC, spectral_range = SpectralRange.RANGE_480_TO_579),
+    MEDR(data_mode = DataMode.SPECTRAL_DATA, data_format = DataFormat.ALPHANUMERIC, spectral_range = SpectralRange.RANGE_580_TO_679),
+    MEDR(data_mode = DataMode.SPECTRAL_DATA, data_format = DataFormat.ALPHANUMERIC, spectral_range = SpectralRange.RANGE_680_TO_780),
+    MEDR(data_mode = DataMode.COLORIMETRIC_DATA, data_format = DataFormat.ALPHANUMERIC),
+    RMTS(switch = ModeSelect.DISABLED)
+]
 
 
 class MeasureDialog(QDialog):
@@ -25,4 +40,5 @@ class MeasureDialog(QDialog):
         self.accept()
 
     def popUp(self):
+        # await self.onAccept()
         self.exec()
