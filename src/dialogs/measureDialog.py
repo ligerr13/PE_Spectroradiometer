a380c7c2e9c3080ab5343870d418aa97b8122716
Horváth Milán <horvathmilan@Horvath-MacBook-Air.local>
2024-04-09 20:20:27 +0200

    Some changes

--- src/dialogs/measureDialog.py
+++ src/dialogs/measureDialog.py
@@ -5,18 +5,26 @@ from src.instrument.command import DataMode, DataFormat, ModeSelect, SpectralRan
 import asyncio, os, re
 
 
-basic_measure_program = [
-    RMTS(switch = ModeSelect.ENABLED),
-    MSWE(switch = ModeSelect.DISABLED),
-    MEAS(switch = ModeSelect.ENABLED),
-    MEDR(data_mode = DataMode.MEASUREMENT_CONDITIONS, data_format = DataFormat.ALPHANUMERIC),
-    MEDR(data_mode = DataMode.SPECTRAL_DATA, data_format = DataFormat.ALPHANUMERIC, spectral_range = SpectralRange.RANGE_380_TO_479),
-    MEDR(data_mode = DataMode.SPECTRAL_DATA, data_format = DataFormat.ALPHANUMERIC, spectral_range = SpectralRange.RANGE_480_TO_579),
-    MEDR(data_mode = DataMode.SPECTRAL_DATA, data_format = DataFormat.ALPHANUMERIC, spectral_range = SpectralRange.RANGE_580_TO_679),
-    MEDR(data_mode = DataMode.SPECTRAL_DATA, data_format = DataFormat.ALPHANUMERIC, spectral_range = SpectralRange.RANGE_680_TO_780),
-    MEDR(data_mode = DataMode.COLORIMETRIC_DATA, data_format = DataFormat.ALPHANUMERIC),
-    RMTS(switch = ModeSelect.DISABLED)
-]
+class MeasureProgramBuilder:
+    def __init__(self):
+        self.measure_program = []
+
+    def enable_measurement_conditions(self):
+        self.measure_program.append(MEAS(switch=ModeSelect.ENABLED))
+
+    def enable_spectral_data(self):
+        self.measure_program.append(MEDR(data_mode=DataMode.SPECTRAL_DATA, data_format=DataFormat.ALPHANUMERIC, spectral_range=SpectralRange.RANGE_380_TO_479))
+        self.measure_program.append(MEDR(data_mode=DataMode.SPECTRAL_DATA, data_format=DataFormat.ALPHANUMERIC, spectral_range=SpectralRange.RANGE_480_TO_579))
+        self.measure_program.append(MEDR(data_mode=DataMode.SPECTRAL_DATA, data_format=DataFormat.ALPHANUMERIC, spectral_range=SpectralRange.RANGE_580_TO_679))
+        self.measure_program.append(MEDR(data_mode=DataMode.SPECTRAL_DATA, data_format=DataFormat.ALPHANUMERIC, spectral_range=SpectralRange.RANGE_680_TO_780))
+
+    def enable_colorimetric_data(self):
+        self.measure_program.append(MEDR(data_mode=DataMode.COLORIMETRIC_DATA, data_format=DataFormat.ALPHANUMERIC))
+
+    def build(self):
+        self.measure_program.insert(0, RMTS(switch=ModeSelect.ENABLED))
+        self.measure_program.append(RMTS(switch=ModeSelect.DISABLED))
+        return self.measure_program
 
 class FileValidator:
     @classmethod
@@ -32,6 +40,13 @@ class MeasureDialog(QDialog):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
 
+        self.measurement_conditions_checkBox = self.ui.checkBox
+        self.spectral_data_checkBox = self.ui.checkBox_2
+        self.colorimetric_data_checkBox = self.ui.checkBox_3
+
+        self.timer = self.ui.spinBox
+        self.times = self.ui.doubleSpinBox
+
     def closeEvent(self, event):
         if self.result() == QDialog.DialogCode.Accepted:
             print("Dialog accepted")
@@ -40,19 +55,30 @@ class MeasureDialog(QDialog):
         event.accept()
 
     def onAccept(self):
-            file_name = self.ui.fileName.text()
-            if len(file_name) < 5:
-                QMessageBox.warning(self, "Warning", "File name must be at least 5 characters long!")
-                return
-            elif not re.match(r'^[a-zA-Z0-9]*$', file_name):
-                QMessageBox.warning(self, "Warning", "File name must contain at least one letter or number!")
-                return
-            elif os.path.exists(os.path.join(FileValidator.get_data_directory(), file_name + ".json")):
-                QMessageBox.warning(self, "Warning", "File already exists in folder:\n" +  FileValidator.get_data_directory())
-                return
-            
-            asyncio.run(ExecuteProgram.run_program(basic_measure_program, file_name))
-            self.accept()
+        file_name = self.ui.fileName.text()
+
+        if len(file_name) < 5:
+            QMessageBox.warning(self, "Warning", "File name must be at least 5 characters long!")
+            return
+        elif not re.match(r'^[a-zA-Z0-9]*$', file_name):
+            QMessageBox.warning(self, "Warning", "File name must contain at least one letter or number!")
+            return
+        elif os.path.exists(os.path.join(FileValidator.get_data_directory(), file_name + ".json")):
+            QMessageBox.warning(self, "Warning", "File already exists in folder:\n" +  FileValidator.get_data_directory())
+            return
+        
+        measure_program_builder = MeasureProgramBuilder()
+        if self.measurement_conditions_checkBox.isChecked():
+            measure_program_builder.enable_measurement_conditions()
+        if self.spectral_data_checkBox.isChecked():
+            measure_program_builder.enable_spectral_data()
+        if self.colorimetric_data_checkBox.isChecked():
+            measure_program_builder.enable_colorimetric_data()
+        
+        measure_program = measure_program_builder.build()
+
+        asyncio.run(ExecuteProgram.run_program(measure_program, file_name))
+        self.accept()
 
     def cleanUp(self):
         self.ui.fileName.setText("")
