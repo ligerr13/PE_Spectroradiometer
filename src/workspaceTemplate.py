f6dba7f7b87fa824008dafda33ed8453cd194ef6
Horváth Milán <horvathmilan@horvath-air.t.hu>
2024-05-18 12:39:49 +0200

    Some changes

--- src/workspaceTemplate.py
+++ src/workspaceTemplate.py
@@ -94,7 +94,7 @@ class WorkspaceDesignWidget(QWidget):
          self.ui.WorkspaceMenuWidget.setVisible(state)
          self.ui.widget_6.setStyleSheet("""QWidget {border: 0;}
                  QPushButton {
-                font: 630 8pt "Arial";
+                font: 630 12pt "Consolas";
                 color: 	rgb(190, 190, 190);
                 padding: 5 5 5 5;
                 margin: 5 5 5 5;
@@ -125,7 +125,7 @@ class WorkspaceDesignWidget(QWidget):
             self.ui.WorkspaceMenuWidget.setVisible(state)
             self.ui.widget_6.setStyleSheet("""QWidget {border: 0;}
                 QPushButton {
-                font: 630 8pt "Arial";
+                font: 630 12pt "Consolas";
                 color: 	rgb(190, 190, 190);
                 padding: 5 5 5 5;
                 margin: 5 5 5 5;
